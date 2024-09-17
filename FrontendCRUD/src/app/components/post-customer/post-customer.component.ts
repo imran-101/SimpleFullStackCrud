1fb389195f8830d3fc5dcd9772939190c827ebe4
MD IMRAN <mdimran201002@gmail.com>
2024-09-17 21:46:15 +0530

    frontendCrud

---
+++ FrontendCRUD/src/app/components/post-customer/post-customer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-customer',
+  templateUrl: './post-customer.component.html',
+  styleUrl: './post-customer.component.css'
+})
+export class PostCustomerComponent {
+
+}
